d8db9997a506fab0334c0252c562a896c6559494
lithhalim <103363189+lithhalim@users.noreply.github.com>
2022-10-16 09:30:52 +0300

    lith

--- src/routes/postes_routes/postes_routes.js
+++ src/routes/postes_routes/postes_routes.js
@@ -1,31 +1,31 @@
-const express = require('express')
-const router = express.Router()
+// const express = require('express')
+// const router = express.Router()
 
 
-//ALL MEDILWARE FUNCTION ARE USE
+// //ALL MEDILWARE FUNCTION ARE USE
 
 
-//------------------------------------ postes all data ----------------------------------------//
-const Create_post_controllers=require("../../controllers/postes-controllers/create-post");
-router.post("/createpost",Create_post_controllers)
+// //------------------------------------ postes all data ----------------------------------------//
+// const Create_post_controllers=require("../../controllers/postes-controllers/create-post");
+// router.post("/createpost",Create_post_controllers)
 
-const Get_all_post=require("../../controllers/postes-controllers/get-allpostes");
-router.get("/getpostes",Get_all_post)
-
-
-const get_page_product=require("../../controllers/postes-controllers/get-Product");
-router.get("/getproductpage/:data",get_page_product)
+// const Get_all_post=require("../../controllers/postes-controllers/get-allpostes");
+// router.get("/getpostes",Get_all_post)
 
 
-
-//--------------------------------------like data ------------------------------------------------//
-const create_like_controllers=require("../../controllers/postes-controllers/like-comment/like/create-like")
-router.post("/createlike",create_like_controllers)
+// const get_page_product=require("../../controllers/postes-controllers/get-Product");
+// router.get("/getproductpage/:data",get_page_product)
 
 
 
-//-----------------------------------comment data -------------------------------------------------//
-const create_comment_controllers =require("../../controllers/postes-controllers/like-comment/comment/create-comment");
-router.post("/createComment",create_comment_controllers)
+// //--------------------------------------like data ------------------------------------------------//
+// const create_like_controllers=require("../../controllers/postes-controllers/like-comment/like/create-like")
+// router.post("/createlike",create_like_controllers)
 
-module.exports=router
+
+
+// //-----------------------------------comment data -------------------------------------------------//
+// const create_comment_controllers =require("../../controllers/postes-controllers/like-comment/comment/create-comment");
+// router.post("/createComment",create_comment_controllers)
+
+// module.exports=router
